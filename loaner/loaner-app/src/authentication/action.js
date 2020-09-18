dfb6719d114e30e0831296df567106599818f92a
mac <mac@macs-MacBook-Pro-2.local>
2020-09-18 12:26:59 +0100

    updated folder

--- loaner/loaner-app/src/authentication/action.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { USER_LOADING, USER_LOADED, AUTH_ERROR } from "../rootAction/types";
-import { BASE_URL } from "../rootAction/env";
-
-// CHECK TOKEN & LOAD USER
-export const loadUser = () => (dispatch, getState) => {
-  dispatch({ type: USER_LOADING });
-};
--- loaner/loaner-app/src/rootReducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import authentication from "../authentication/reducer";
-
-export default combineReducers({
-  authentication,
-});
